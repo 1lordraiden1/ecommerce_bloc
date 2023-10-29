8e969126a75e448f8f0cc9d87a7e548adda46312
lordraiden <lordraiden101@gmail.com>
2023-10-29 11:15:43 +0200

    New Events and States have been added

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grocery/features/home/ui/bloc/home_bloc.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,12 +10,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final HomeBloc homeBloc = HomeBloc();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Grocery App"),
-      ),
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      // listenWhen: (previous, current) {},
+      // buildWhen: (previous, current) {},
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Grocery App"),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border_outlined),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.shopping_cart),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
