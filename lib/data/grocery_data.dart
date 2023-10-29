ea5106a766e1744df7269843ac97553726b2cdb0
lordraiden <lordraiden101@gmail.com>
2023-10-29 10:25:42 +0200

    data gathered successfully

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,72 @@
+List<Map<String, dynamic>> groceryProducts = [
+  {
+    "id": 1,
+    "name": "Apples",
+    "price": 1.99,
+    "description": "Fresh and crisp apples.",
+    "imageURL":
+        "https://hips.hearstapps.com/hmg-prod/images/red-fresh-apple-isolated-on-white-background-royalty-free-image-1627314996.jpg?crop=1.00xw:0.923xh;0,0.0486xh&resize=980:*",
+  },
+  {
+    "id": 2,
+    "name": "Milk",
+    "price": 2.49,
+    "description": "1 gallon of whole milk.",
+    "imageURL":
+        "https://cdn.store-factory.com/www.culinaide.com/content/product_4801045b.jpg?v=1597660182",
+  },
+  {
+    "id": 3,
+    "name": "Bread",
+    "price": 2.29,
+    "description": "Artisanal wheat bread loaf.",
+    "imageURL":
+        "https://assets.bonappetit.com/photos/5c62e4a3e81bbf522a9579ce/16:9/w_4000,h_2250,c_limit/milk-bread.jpg",
+  },
+  {
+    "id": 4,
+    "name": "Eggs",
+    "price": 1.99,
+    "description": "One dozen large eggs.",
+    "imageURL":
+        "https://img.taste.com.au/F0W1xU4i/taste/2018/02/cls0318eggss_eggs-135792-1.jpg",
+  },
+  {
+    "id": 5,
+    "name": "Chicken Breast",
+    "price": 5.99,
+    "description": "Boneless, skinless chicken breasts.",
+    "imageURL":
+        "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2023/01/Air-Fryer-Chicken-Breast-main.jpg",
+  },
+  {
+    "id": 6,
+    "name": "Cereal",
+    "price": 3.49,
+    "description": "Healthy breakfast cereal.",
+    "imageURL": "https://m.media-amazon.com/images/I/81ZEYqrV1IL.jpg",
+  },
+  {
+    "id": 7,
+    "name": "Spinach",
+    "price": 1.79,
+    "description": "Fresh and nutritious spinach.",
+    "imageURL": "https://freshji.in/wp-content/uploads/2020/09/Spinach-1.jpg",
+  },
+  {
+    "id": 8,
+    "name": "Pasta",
+    "price": 1.29,
+    "description": "Penne pasta, 16 oz.",
+    "imageURL":
+        "https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&resize=556,505",
+  },
+  {
+    "id": 9,
+    "name": "Salmon",
+    "price": 9.99,
+    "description": "Wild-caught salmon fillet.",
+    "imageURL":
+        "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2F2023-04-baked-salmon-how-to%2Fbaked-salmon-step6-4792",
+  },
+];
