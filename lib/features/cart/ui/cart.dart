d0079faaf1d10a26d640174c1e375e80a8cf8db4
lordraiden <lordraiden101@gmail.com>
2023-10-31 10:52:12 +0200

    Update

--- lib/features/cart/ui/cart.dart
+++ lib/features/cart/ui/cart.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grocery/features/cart/bloc/cart_bloc.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -9,7 +11,35 @@ class Cart extends StatefulWidget {
 
 class _CartState extends State<Cart> {
   @override
+  void initState() {
+    cartBloc.add(CartInitialEvent());
+    super.initState();
+  }
+
+  final CartBloc cartBloc = CartBloc();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Cart"),
+        ),
+      ),
+      body: BlocConsumer<CartBloc, CartState>(
+        listener: (context, state) {},
+        listenWhen: (previous, current) => current is CartActionState,
+        buildWhen: (previous, current) => current is! CartActionState,
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case :
+              
+              break;
+            default:
+          }
+          return Container();
+        },
+      ),
+    );
   }
 }
