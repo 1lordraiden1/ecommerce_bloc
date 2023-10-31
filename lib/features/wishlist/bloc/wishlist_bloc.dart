d10dda7332115da162ec16460c437f542bf757ab
lordraiden <lordraiden101@gmail.com>
2023-10-31 13:38:28 +0200

    get wishlist data successfully

--- lib/features/wishlist/bloc/wishlist_bloc.dart
+++ lib/features/wishlist/bloc/wishlist_bloc.dart
@@ -1,13 +1,21 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:grocery/models/home_product_data_model.dart';
 import 'package:meta/meta.dart';
+
+import '../../../data/wishlist_items.dart';
 
 part 'wishlist_event.dart';
 part 'wishlist_state.dart';
 
 class WishlistBloc extends Bloc<WishlistEvent, WishlistState> {
   WishlistBloc() : super(WishlistInitial()) {
-    on<WishlistEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<WishlistInitialEvent>(wishlistInitialEvent);
+  }
+
+  FutureOr<void> wishlistInitialEvent(
+      WishlistInitialEvent event, Emitter<WishlistState> emit) {
+    emit(WishlistSuccessState(wishlistItems));
   }
 }
--- lib/features/wishlist/ui/wishlist.dart
+++ lib/features/wishlist/ui/wishlist.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grocery/features/wishlist/bloc/wishlist_bloc.dart';
 
 class Wishlist extends StatefulWidget {
   const Wishlist({super.key});
@@ -8,8 +10,30 @@ class Wishlist extends StatefulWidget {
 }
 
 class _WishlistState extends State<Wishlist> {
+  final WishlistBloc wishlistBloc = WishlistBloc();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          "WishList",
+          textAlign: TextAlign.center,
+        )),
+      ),
+      body: BlocConsumer<WishlistBloc, WishlistState>(
+        bloc: wishlistBloc,
+        listenWhen: (previous, current) => current is WishlistActionState,
+        buildWhen: (previous, current) => current is !WishlistActionState,
+        listener: (context, state) {
+          
+        },
+        builder: (context, state) {
+
+          
+          return Container();
+        },
+      ),
+    );
   }
 }
